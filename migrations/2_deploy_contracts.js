b93c8013d7a0f54f3c25e775e2f9eb378b60ae12
Rick Farina <rickrfarina@gmail.com>
2022-01-30 00:07:38 -0600

    2_deploy_contracts.js to place on blockchain and transfer eth to ethSwap contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,18 @@
 
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+
+    // Deploy Token
+    await deployer.deploy(Token);
+    const token = await Token.deployed()
+  
+    // Deploy EthSwap
+    await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+
+    // Transfer all tokens to EthSwap contract
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+
 };

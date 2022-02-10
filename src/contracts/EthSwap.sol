dab2add613a799d98735be9ef680dc318341a371
Rick Farina <rickrfarina@gmail.com>
2022-02-09 19:17:35 -0600

    complete EthSwap buyTokens() and sellTokens() with commentary

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -23,7 +23,7 @@ contract EthSwap {
     //           Token contract.
     Token public token;  
 
-    uint public ratePerToken = 100; // 100 DAPP for 1 ether
+    uint256 public ratePerToken = 100; // 100 DAPP for 1 ether
 
     constructor(Token _token) public {
         token = _token;
@@ -31,11 +31,12 @@ contract EthSwap {
     }
 
     // Another account or contract wants to buy DAPP tokens with ether
-    function buyTokens() public payable {
+    // We will transfer tokens to the buyer, who is providing ether
+    function buyTokens() external payable {
 
         // Note: this function does not have input parameters. However, solidity will 
         // automatically provide msg.sender and msg.value as sender's account address 
-        // and amount of ether sent in. (msg, address, and block objects are part of the global
+        // and amount of ether sent in wei. (msg, address, and block objects are part of the global
         // context made available by solidity)
 
         // Also note the use of the payable keyword. This allows this function to accept
@@ -43,15 +44,52 @@ contract EthSwap {
         
         
         // Calculate the number of DAPP tokens the ether will buy
-        uint tokensPurchased = msg.value * ratePerToken;
+        uint256 tokensPurchased = (msg.value/10**18)  * ratePerToken;  // divide by 10**18 to convert wei to ether
 
         // Invoke the transaction on the Token contract
         token.transfer(msg.sender, tokensPurchased);    
+
+        /* 
+        rrf note:
+        =========
+        All of the following to update the balance of the contract IS NOT NEEDED!
+        Solidity has already recognized this as a payment transaction and takes
+        care of and is responsible for adding to the contract's balance.
+         */
+        
+        // Add the funds to the current contract's balance
+        // address payable contractAddress = payable(address(this));
+        // contractAddress.balance += msg.value;
     }
+
+        // Sell tokens back to the exchange in return for ether at the current exchange rate
+        function sellTokens(uint256 numberOfTokensToSell) public {
+
+            uint256 ethOwed = (numberOfTokensToSell / ratePerToken);
+
+            // transfer tokens from msg.sender to this contract
+            // token.transfer(msg.sender, numberOfTokensToTrade);  // this function is not the correct one to invoke
+            token.transferFrom(msg.sender, address(this), numberOfTokensToSell );  // this function transfers token balance to the contract
+
+
+            // transfer ether from this contract to msg.sender
+            msg.sender.transfer(ethOwed); // Contract sending ether to msg.sender
+
+        }
+
+
+
+
+
     // Determine whether address is eoa or contract
     function getAccountType(address addr) public view returns(string memory) {
         uint256 codeLength;
 
+        // if account has data (codeLength > ), then it is a contract, as eoa 
+        // accounts do not have data.
+        // BUT, it is possible for eoa accounts to send data. It is just that eoa 
+        //      accounts do not currently processs that data. SO, this could result
+        //      in false information being sent back to the caller.
         assembly { codeLength := extcodesize(addr) }
         return codeLength == 0 ? 'eoa' : 'contract';
     }
@@ -63,4 +101,11 @@ contract EthSwap {
         return codeLength == 0 ? false : true;
     }
 
+    function getTokenBalance(address _addr) public view returns(uint256) {
+        return (token.balanceOf(_addr));
+    }
+
+    function getEtherBalance(address _addr) public view returns(uint256) {
+        return address(_addr).balance;
+    }
 }
